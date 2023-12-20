c5bb3a5b4848a4339c7a28093415918d745b2e6b
maulanahusains <maulanahusain@proton.me>
2023-12-20 09:36:37 +0700

    feat: add navbar section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bkg: '#F0F0F0', // putih
+        primary: '#213555', // navy
+        secondary: '#4F709C', // biru rada ke navy
+        darkersecondary: '#3f5a7d', // biru rada ke navy pas hover
+        accent: '#E5D283' // yellow
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      fontSize: {
+        title: `2.6rem;`,
+        paragraph: `1rem;`
+      },  
+    },
   },
   plugins: [],
 }
